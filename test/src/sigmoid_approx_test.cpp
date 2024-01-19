0c68d4d17242d707ba07fa7f1901692b7ed72d58
jatinchowdhury18 <jatinchowdhury18@gmail.com>
2024-01-19 00:12:05 -0800

    Add alternative sigmoid approximation (#3)
    * Add sigmoid_exp approximation
    
    * Undo comments
    
    * Tweaking error bounds

--- test/src/sigmoid_approx_test.cpp
+++ test/src/sigmoid_approx_test.cpp
@@ -12,7 +12,7 @@ TEST_CASE ("Sigmoid Approx Test")
     const auto all_floats = test_helpers::all_32_bit_floats (-10.0f, 10.0f, 1.0e-1f);
 #endif
     const auto y_exact = test_helpers::compute_all<float> (all_floats, [] (auto x)
-                                                    { return 1.0f / (1.0f + std::exp (-x)); });
+                                                           { return 1.0f / (1.0f + std::exp (-x)); });
 
     const auto test_approx = [&all_floats, &y_exact] (auto&& f_approx, float err_bound)
     {
@@ -48,5 +48,71 @@ TEST_CASE ("Sigmoid Approx Test")
         test_approx ([] (auto x)
                      { return math_approx::sigmoid<3> (x); },
                      2.0e-3f);
+    }
+}
+
+TEST_CASE ("Sigmoid (Exp) Approx Test")
+{
+#if ! defined(WIN32)
+    const auto all_floats = test_helpers::all_32_bit_floats (-10.0f, 10.0f, 1.0e-3f);
+#else
+    const auto all_floats = test_helpers::all_32_bit_floats (-10.0f, 10.0f, 1.0e-1f);
+#endif
+    const auto y_exact = test_helpers::compute_all<float> (all_floats, [] (auto x)
+                                                           { return 1.0f / (1.0f + std::exp (-x)); });
+
+    const auto test_approx = [&all_floats, &y_exact] (auto&& f_approx, float err_bound, float rel_err_bound, uint32_t ulp_bound)
+    {
+        const auto y_approx = test_helpers::compute_all<float> (all_floats, f_approx);
+
+        const auto error = test_helpers::compute_error<float> (y_exact, y_approx);
+        const auto rel_error = test_helpers::compute_rel_error<float> (y_exact, y_approx);
+        const auto ulp_error = test_helpers::compute_ulp_error (y_exact, y_approx);
+
+        const auto max_error = test_helpers::abs_max<float> (error);
+        const auto max_rel_error = test_helpers::abs_max<float> (rel_error);
+        const auto max_ulp_error = *std::max_element (ulp_error.begin(), ulp_error.end());
+
+        std::cout << max_error << ", " << max_rel_error << ", " << max_ulp_error << std::endl;
+        REQUIRE (std::abs (max_error) < err_bound);
+        REQUIRE (std::abs (max_rel_error) < rel_err_bound);
+        if (ulp_bound > 0)
+            REQUIRE (max_ulp_error < ulp_bound);
+    };
+
+    SECTION ("6th-Order (Exp)")
+    {
+        test_approx ([] (auto x)
+                     { return math_approx::sigmoid_exp<6> (x); },
+                     1.5e-7f,
+                     6.5e-7f,
+                     12);
+    }
+
+    SECTION ("5th-Order (Exp)")
+    {
+        test_approx ([] (auto x)
+                     { return math_approx::sigmoid_exp<5> (x); },
+                     1.5e-7f,
+                     7.5e-7f,
+                     12);
+    }
+
+    SECTION ("4th-Order (Exp)")
+    {
+        test_approx ([] (auto x)
+                     { return math_approx::sigmoid_exp<4> (x); },
+                     9.5e-7f,
+                     4.5e-6f,
+                     65);
+    }
+
+    SECTION ("3rd-Order (Exp)")
+    {
+        test_approx ([] (auto x)
+                     { return math_approx::sigmoid_exp<3> (x); },
+                     3.0e-4f,
+                     1.5e-4f,
+                     0);
     }
 }
--- tools/bench/sigmoid_bench.cpp
+++ tools/bench/sigmoid_bench.cpp
@@ -28,6 +28,9 @@ SIGMOID_BENCH (sigmoid_std, [] (auto x) { return 1.0f / (1.0f + std::exp (-x)); 
 SIGMOID_BENCH (sigmoid_approx9, math_approx::sigmoid<9>)
 SIGMOID_BENCH (sigmoid_approx7, math_approx::sigmoid<7>)
 SIGMOID_BENCH (sigmoid_approx5, math_approx::sigmoid<5>)
+SIGMOID_BENCH (sigmoid_exp_approx6, math_approx::sigmoid_exp<6>)
+SIGMOID_BENCH (sigmoid_exp_approx5, math_approx::sigmoid_exp<5>)
+SIGMOID_BENCH (sigmoid_exp_approx4, math_approx::sigmoid_exp<4>)
 
 #define SIGMOID_SIMD_BENCH(name, func) \
 void name (benchmark::State& state) \
@@ -47,5 +50,8 @@ SIGMOID_SIMD_BENCH (sigmoid_xsimd, [] (auto x) { return 1.0f / (1.0f + xsimd::ex
 SIGMOID_SIMD_BENCH (sigmoid_simd_approx9, math_approx::tanh<9>)
 SIGMOID_SIMD_BENCH (sigmoid_simd_approx7, math_approx::tanh<7>)
 SIGMOID_SIMD_BENCH (sigmoid_simd_approx5, math_approx::tanh<5>)
+SIGMOID_SIMD_BENCH (sigmoid_exp_simd_approx6, math_approx::sigmoid_exp<6>)
+SIGMOID_SIMD_BENCH (sigmoid_exp_simd_approx5, math_approx::sigmoid_exp<5>)
+SIGMOID_SIMD_BENCH (sigmoid_exp_simd_approx4, math_approx::sigmoid_exp<4>)
 
 BENCHMARK_MAIN();
